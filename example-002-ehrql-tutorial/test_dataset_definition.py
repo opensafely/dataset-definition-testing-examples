fd040eabad687399031a15eb0eea85db1492c733
Eli <94979642+eli-miriam@users.noreply.github.com>
2025-07-07 09:53:22 +0000

    Fill out more examples for ehrQL tutorial

--- example-002-ehrql-tutorial/test_dataset_definition.py
+++ example-002-ehrql-tutorial/test_dataset_definition.py
@@ -2,7 +2,7 @@ from datetime import date
 from dataset_definition import dataset
 
 test_data = {
-    # Expected in population with unresolved diabetes and a proteinuria diagnosis
+    # Expected in population, with unresolved diabetes and a proteinuria diagnosis
     1: {
         "patients": {"date_of_birth": date(1970, 1, 1), "sex": "female"},
         "medications": [],
@@ -28,7 +28,7 @@ test_data = {
             "ace_or_arb": False,
         },
     },
-    # Not expected in population, due to revolved diabetes that unresolved AFTER the index date
+    # Not expected in population, due to revolved diabetes that unresolved AFTER the index date.
     2: {
         "patients": {"date_of_birth": date(1950, 1, 1), "sex": "female"},
         "medications": [],
@@ -60,16 +60,77 @@ test_data = {
         ],
         "expected_in_population": False,
     },
-    # Expected in population with [criteria]
+    # Expected in population with unresolved diabetes, a diagnosis of 
+    # # micro-albuminuria, and a recent history of treatment with ACE medications
     3: {
-        "patients": [{"date_of_birth": date(1950, 1, 1), "sex": "male"}],
-        "medications": [],
-        "clinical_events": [],
-        "practice_registrations": [],
-        "expected_in_population": False,
+        "patients": [{"date_of_birth": date(1973, 5, 1), "sex": "intersex"}],
+        "medications": [
+            {
+                # ACE prescription within 180 days of index date
+                "date": date(2024, 1, 1),
+                "dmd_code": "21912111000001107",
+            }
+        ],
+        "clinical_events": [
+            {
+                # Diabetes diagnosis
+                "date": date(2016, 1, 1),
+                "snomedct_code": "44054006",
+            },
+            {
+                # MAL diagnosis
+                "date": date(2017, 1, 1),
+                "snomedct_code": "762261001",
+            }
+        ],
+        "practice_registrations": [
+            {
+                "start_date": date(2013, 7, 10),
+                "practice_pseudo_id": 90210,
+            }],
+        "expected_in_population": True,
+        "expected_columns": {
+            "prt_or_mal": True,
+            "ace_or_arb": True,
+        },
     },
-    # Not expected in population
+    # Expected in population with unresolved diabetes and a diagnosis of 
+    # # micro-albuminuria. However, their history of ACE inhibitor treatment is not recent 
+    # enough to count.
     4: {
+        "patients": [{"date_of_birth": date(1973, 5, 1), "sex": "intersex"}],
+        "medications": [
+            {
+                # ACE prescription, earlier than 180 days of index date
+                "date": date(2018, 1, 1),
+                "dmd_code": "21912111000001107",
+            }
+        ],
+        "clinical_events": [
+            {
+                # Diabetes diagnosis
+                "date": date(2016, 1, 1),
+                "snomedct_code": "44054006",
+            },
+            {
+                # MAL diagnosis
+                "date": date(2017, 1, 1),
+                "snomedct_code": "762261001",
+            }
+        ],
+        "practice_registrations": [
+            {
+                "start_date": date(2013, 7, 10),
+                "practice_pseudo_id": 90210,
+            }],
+        "expected_in_population": True,
+        "expected_columns": {
+            "prt_or_mal": True,
+            "ace_or_arb": False,
+        },
+    },
+    # Not expected in population due to lack of practice registration
+    5: {
         "patients": [{"date_of_birth": date(2010, 1, 1)}],
         "medications": [],
         "clinical_events": [],
